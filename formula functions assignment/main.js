ba7238fec995eaff4744fc364d4165455ffb0461
forampatel2 <78555940+forampatel2@users.noreply.github.com>
2021-02-10 11:10:06 -0700

    Add files via upload

---
+++ formula functions assignment/main.js
@@ -0,0 +1,33 @@
+// Farenheit into Celsius
+
+//HTML Elements
+let calcBtnEl = document.getElementById("calc");
+let num1InEl = document.getElementById("num1");
+let num2InEl = document.getElementById("num2");
+let num3InEl = document.getElementById("num3");
+let num4InEl = document.getElementById("num4");
+let slopeOutEl = document.getElementById("slope-out");
+
+// Event Listener
+calcBtnEl.addEventListener("click", calcSlope);
+
+// Event Function
+function calcSlope(){
+    // Input
+let num1 = +inputEl1.value;
+let num2 = +inputEl2.value;
+let num3  = +inputEl3.value;
+let num4 = +inputEl4.value;
+
+// Output
+slopeOutEL.innerHTML = formulaFunction()
+}
+
+// Calculate and return the slope
+function slopeFormula(x1, y1, x2, y2) {
+    let s = (y2 - y1) / (x2 - x1);
+return Math.( )
+
+
+}
+
